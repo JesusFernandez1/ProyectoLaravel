2cdf16bfc731fec2ccda3d0e63578f0f258731a3
Jesus <jesus20102000@gmail.com>
2023-01-21 18:11:42 +0100

    commit

---
+++ app/Http/Controllers/feesController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class cuotasController extends Controller
+{
+    public function verCrearcliente() {
+        return view('clientes.clientes_crear');
+    }
+
+    public function crearcliente() {
+
+        //crea el cliente
+
+        return view('clientes.clientes_mostrar');
+    }
+
+    public function verModificarcliente($id) {
+        return view('clientes.clientes_modificar, ["id => $id]');
+    }
+
+    public function modificarcliente($id) {
+        return view('clientes.clientes_mostrar');
+    }
+
+    public function verEliminarcliente($id) {
+        return view('clientes.clientes_eliminar, ["id => $id]');
+    }
+
+    public function comprobarBorrarcliente($id) {
+        
+        if ("comprobar") {
+            //borrarcliente($id);
+            return view('clientes.clientes_mostrar');
+        } else {
+            return view('clientes.clientes_eliminar');
+        }
+
+    }
+
+    function borrarcliente($id) {
+        
+    }
+
+    public function filtradoCredenciales() {
+        
+    }
+}
