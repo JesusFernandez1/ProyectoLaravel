c552ce87a7ecfcb3ae52fdcde0385d02b40b2e5a
Jesus <jesus20102000@gmail.com>
2023-02-19 15:56:09 +0100

    commit

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\taskController;
 use App\Http\Controllers\customerController;
 use App\Http\Controllers\feeController;
 use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,19 +30,19 @@ Route::get('/', function () {
     return view('base');
 })->middleware(['auth', 'verified'])->name('base');
 
-Route::controller(userController::class)->group(function(){
+Route::controller(userController::class)->group(function () {
     Route::get('usuarios/usuarios_eliminada/{id}', 'confirmarEliminarUsuario')->middleware('auth')->name('usuarios.confirmarEliminarUsuario');
 });
 Route::resource('usuarios', userController::class)->middleware('auth');
 
-Route::controller(customerController::class)->group(function(){
+Route::controller(customerController::class)->group(function () {
     Route::get('clientes/clientes_crearIncidencia', 'nuevaIncidencia')->name('clientes.nuevaIncidencia');
     Route::post('clientes/clientes_incidenciaCreada', 'crearIncidencia')->name('clientes.crearIncidencia');
     Route::get('clientes/clientes_eliminado/{id}', 'confirmarEliminarCliente')->middleware('auth')->middleware('admin')->name('clientes.confirmarEliminarCliente');
 });
 Route::resource('clientes', customerController::class)->middleware('auth')->middleware('admin');
 
-Route::controller(feeController::class)->group(function(){
+Route::controller(feeController::class)->group(function () {
     Route::get('cuotas/cuotas_excepcional/{id}', 'agregar')->middleware('auth')->middleware('admin')->name('cuotas.agregar');
     Route::get('cuotas/cuotas_excepcionalCreada/{id}', 'agregar')->middleware('auth')->middleware('admin')->name('cuotas.agregar');
     Route::get('cuotas/cuotas_eliminar/{id}', 'mostrarEliminar')->middleware('auth')->middleware('admin')->name('cuotas.mostrarEliminar');
@@ -50,7 +52,7 @@ Route::controller(feeController::class)->group(function(){
 });
 Route::resource('cuotas', feeController::class)->middleware('auth')->middleware('admin');
 
-Route::controller(taskController::class)->group(function(){
+Route::controller(taskController::class)->group(function () {
     Route::get('tareas/tareas_verInformacionDetallada', 'verInformacionDetallada')->middleware('auth')->name('tareas.verInformacionDetallada');
     Route::get('tareas/tareas_pendientes', 'verTareasPendientes')->middleware('auth')->name('tareas.verTareasPendientes');
     Route::get('tareas/tareas_mostrarNoAsignadas', 'verTareasNoAsignadas')->middleware('auth')->middleware('admin')->name('tareas.verTareasNoAsignadas');
@@ -59,7 +61,6 @@ Route::controller(taskController::class)->group(function(){
     Route::get('tareas/tareas_eliminada/{id}', 'confirmarBorrarTarea')->middleware('auth')->middleware('admin')->name('tareas.confirmarBorrarTarea');
     Route::get('tareas/tareas_completar/{id}', 'cambiarEstadoTarea')->middleware('auth')->middleware('operario')->name('tareas.cambiarEstadoTarea');
     Route::put('tareas/tareas_completada/{id}', 'completarTarea')->middleware('auth')->middleware('operario')->name('tareas.completarTarea');
-    
 });
 Route::resource('tareas', taskController::class)->middleware('auth');
 
@@ -72,11 +73,29 @@ Route::resource('tareas', taskController::class)->middleware('auth');
 Route::get('/auth/github/redirect', function () {
     return Socialite::driver('github')->redirect();
 });
- 
+
 Route::get('/auth/github/callback', function () {
-    $user = Socialite::driver('github')->user();
- 
-    // $user->token
+    $githubuser = Socialite::driver('github')->user();
+
+    $user = User::updateOrcreate(
+        [
+            'provider_id' => $githubuser->getId()
+        ],
+        [
+            'email' => $githubuser->getEmail(),
+            'name' => $githubuser->getName(),
+        ]
+    );
+
+    // $user = User::create([
+    //     'email' => $githubuser->getEmail(),
+    //     'name' => $githubuser->getName(),
+    //     'provider_id' => $githubuser->getId()
+    // ]);
+
+    Auth::login($user);
+
+    return redirect('/base');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
