c4aa9a5561c3aebb82d3e7e91f3f34c50310777c
Jesus <jesus20102000@gmail.com>
2023-02-20 19:30:04 +0100

    commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -45,7 +45,9 @@
             <a href="{{ route('github.redirectGithub') }}" class="ml-3 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Github</a>
         </div>
     </form>
-    <x-primary-button class="ml-3">
-        <a href="{{ route('clientes.nuevaIncidencia') }}" class="nav-link px-2 text-white">Crear incidencia</a>
-    </x-primary-button>
+    <div class="mt-4 flex justify-center">
+        <x-primary-button>
+            <a href="{{ route('clientes.nuevaIncidencia') }}" class="nav-link px-2 text-white">Crear incidencia</a>
+        </x-primary-button>
+    </div>
 </x-guest-layout>
