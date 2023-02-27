ba5fa88ae8b1efebc4ac7af86ab19928a3bbf575
Jesus <jesus20102000@gmail.com>
2023-02-27 16:37:44 +0100

    commit

--- tests/Feature/RouteTest.php
+++ tests/Feature/RouteTest.php
@@ -18,33 +18,25 @@ class RouteTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertStatus(302); 
-        $response->assertRedirect('/login'); 
-
+        $response->assertStatus(302);
+        $response->assertRedirect('/login');
     }
 
     public function test_login()
-{
-    // Crear un usuario
-    $user = User::factory()->create([
-        'email' => 'test@example.com',
-        'password' => bcrypt('password'),
-    ]);
+    {
+        $user = User::factory()->create([
+            'email' => 'test@example.com',
+            'password' => bcrypt('password'),
+        ]);
 
-    // // Iniciar sesión como el usuario
-    // $response = $this->post('/login', [
-    //     'email' => 'test@example.com',
-    //     'password' => 'password',
-    // ]);
+        $this->get('/login')->assertSee('Login');
+        $credentials = [
+            "email" => $user->email,
+            "password" => $user->password
+        ];
 
-    // Comprobar que se redirecciona correctamente después del inicio de sesión
-    // $response->assertRedirect('/');
-
-    // Comprobar que el usuario está autenticado
-    $this->assertAuthenticatedAs($user);
-
-    // Comprobar que la ruta /dashboard está protegida y que se puede acceder después de iniciar sesión
-    $response = $this->get('/');
-    $response->assertStatus(302);
-}
+        $response = $this->post('/login', $credentials);
+        $response->assertRedirect('/');
+        $this->assertCredentials($credentials);
+    }
 }
